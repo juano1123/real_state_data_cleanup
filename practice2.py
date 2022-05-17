6ef706c7adffee53103e068b5208dd489db5bd77
juano1123 <juanandresvega23@gmail.com>
2022-05-17 20:38:07 +0000

    Update pre work Juan Andres Vega

--- practice2.py
+++ practice2.py
@@ -20,8 +20,8 @@ min_index_surface = columnSurface.idxmin()
 min_value_surface = columnSurface.min()
 
 #Solutions
+print(columns)
 print(f"The id of the most expensive house is {max_index_price}")
 print(f"The id of the cheapest house is {min_index_price}")
 print(f"The id of the smallest house is {min_index_surface} and has a surface of {min_value_surface}")
 print(f"The id of the biggest house is {max_index_surface} and has a surface of {max_value_surface}")
-print(columns)
